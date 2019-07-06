ac8fb060acf44ae8191df58868a1424c9218b162
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2019-07-06 13:32:18 -0500

    Removed unnecessary mappings from Kyber Swap contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,8 @@
 import {
-  ExecuteTrade as ExecuteTradeEvent,
-  KyberNetworkSet as KyberNetworkSetEvent,
-  TokenWithdraw as TokenWithdrawEvent,
-  EtherWithdraw as EtherWithdrawEvent,
-  TransferAdminPending as TransferAdminPendingEvent,
-  AdminClaimed as AdminClaimedEvent,
-  AlerterAdded as AlerterAddedEvent,
-  OperatorAdded as OperatorAddedEvent
+  ExecuteTrade as ExecuteTradeEvent
 } from "../generated/Contract/Contract"
 import {
-  ExecuteTrade,
-  KyberNetworkSet,
-  TokenWithdraw,
-  EtherWithdraw,
-  TransferAdminPending,
-  AdminClaimed,
-  AlerterAdded,
-  OperatorAdded
+  ExecuteTrade
 } from "../generated/schema"
 
 export function handleExecuteTrade(event: ExecuteTradeEvent): void {
@@ -28,70 +14,5 @@ export function handleExecuteTrade(event: ExecuteTradeEvent): void {
   entity.dest = event.params.dest
   entity.actualSrcAmount = event.params.actualSrcAmount
   entity.actualDestAmount = event.params.actualDestAmount
-  entity.save()
-}
-
-export function handleKyberNetworkSet(event: KyberNetworkSetEvent): void {
-  let entity = new KyberNetworkSet(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.newNetworkContract = event.params.newNetworkContract
-  entity.oldNetworkContract = event.params.oldNetworkContract
-  entity.save()
-}
-
-export function handleTokenWithdraw(event: TokenWithdrawEvent): void {
-  let entity = new TokenWithdraw(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.token = event.params.token
-  entity.amount = event.params.amount
-  entity.sendTo = event.params.sendTo
-  entity.save()
-}
-
-export function handleEtherWithdraw(event: EtherWithdrawEvent): void {
-  let entity = new EtherWithdraw(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.amount = event.params.amount
-  entity.sendTo = event.params.sendTo
-  entity.save()
-}
-
-export function handleTransferAdminPending(
-  event: TransferAdminPendingEvent
-): void {
-  let entity = new TransferAdminPending(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.pendingAdmin = event.params.pendingAdmin
-  entity.save()
-}
-
-export function handleAdminClaimed(event: AdminClaimedEvent): void {
-  let entity = new AdminClaimed(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.newAdmin = event.params.newAdmin
-  entity.previousAdmin = event.params.previousAdmin
-  entity.save()
-}
-
-export function handleAlerterAdded(event: AlerterAddedEvent): void {
-  let entity = new AlerterAdded(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.newAlerter = event.params.newAlerter
-  entity.isAdd = event.params.isAdd
-  entity.save()
-}
-
-export function handleOperatorAdded(event: OperatorAddedEvent): void {
-  let entity = new OperatorAdded(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.newOperator = event.params.newOperator
-  entity.isAdd = event.params.isAdd
   entity.save()
 }
